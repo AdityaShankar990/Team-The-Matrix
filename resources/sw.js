b272b9ea027a3ed3c67a4b4d87f6e14b83c79100
neo89566 <abhijitkumar9608@gmail.com>
2026-08-20 16:04:07 +0530

    Update sw.js

--- resources/sw.js
+++ resources/sw.js
@@ -7,3 +7,14 @@ const STALE_WHILE_REVALIDATE_RE = /\/assets\/data\//;
 self.addEventListener("install", () => {
 	self.skipWaiting();
 });
+self.addEventListener("activate", (event) => {
+	event.waitUntil(
+		(async () => {
+			// Clean up any caches left over from a previous CACHE_NAME.
+			const keys = await caches.keys();
+			await Promise.all(
+				keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+			);
+			await self.clients.claim();
+		})()
+	);
